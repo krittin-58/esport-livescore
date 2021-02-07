1072d9006ba38a4cc7bfbe50410f83b052bc3b59
Krittin Jaruvisut <krittin.j58@rsu.ac.th>
2021-02-07 21:16:46 +0700

    used private cors proxy

--- src/config/api.js
+++ src/config/api.js
@@ -2,20 +2,20 @@
 
 const API_URL_ENDPOINT = 'https://api.pandascore.co';
 const ACCESS_TOKEN = 'd0Q0tNZWxtU_f_wpHcGBfi8bHD1X3SkVaUZBMQVt_m5xc18ZDjs';
+const PROXY_URL = 'https://esport-livescore.herokuapp.com';
 
 // PATH
 const LEAGUES = 'leagues';
 
 const headers = {
     'content-type': 'application/json',
-    'Access-Control-Allow-Origin': '*',
-    mode: 'cors'
+    'Access-Control-Allow-Origin': '*'
 }
 
 const PandascoreAPI = {
     // Get List Leagues
     async getListLeagues() {
-        const response = await fetch(`${API_URL_ENDPOINT}/${LEAGUES}?token=${ACCESS_TOKEN}`, headers);
+        const response = await fetch(`${PROXY_URL}/${API_URL_ENDPOINT}/${LEAGUES}?token=${ACCESS_TOKEN}`, headers);
         return response.json();
     }
 }
