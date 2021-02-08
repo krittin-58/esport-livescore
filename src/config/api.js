9919edccd9184860b28b02212b4d0ae9830c279e
Krittin Jaruvisut <krittin.j58@rsu.ac.th>
2021-02-08 12:08:09 +0700

    unused proxy url on headers

--- src/config/api.js
+++ src/config/api.js
@@ -2,21 +2,21 @@
 
 const API_URL_ENDPOINT = 'https://api.pandascore.co';
 const ACCESS_TOKEN = 'd0Q0tNZWxtU_f_wpHcGBfi8bHD1X3SkVaUZBMQVt_m5xc18ZDjs';
-const PROXY_URL = 'https://esport-livescore.herokuapp.com';
+// const PROXY_URL = 'https://esport-livescore.herokuapp.com';
 
 // PATH
 const LEAGUES = 'leagues';
 
 const headers = {
     'content-type': 'application/json',
-    'Access-Control-Allow-Origin': 'https://esport-livescore-362bf.web.app',
-    mode: 'cors'
+    // 'Access-Control-Allow-Origin': 'https://esport-livescore-362bf.web.app',
+    // mode: 'cors'
 }
 
 const PandascoreAPI = {
     // Get List Leagues
     async getListLeagues() {
-        const response = await fetch(`${PROXY_URL}/${API_URL_ENDPOINT}/${LEAGUES}?token=${ACCESS_TOKEN}`, headers);
+        const response = await fetch(`${API_URL_ENDPOINT}/${LEAGUES}?token=${ACCESS_TOKEN}`, headers);
         return response.json();
     }
 }
