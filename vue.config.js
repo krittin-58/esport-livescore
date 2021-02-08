b7de5af19be2c7b8defc582c9583973603dba73e
Krittin Jaruvisut <krittin.j58@rsu.ac.th>
2021-02-08 12:05:59 +0700

    changed proxy vue.config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  devServer: {
+    proxy: {
+      '^/': {
+        target: 'https://api.pandascore.co',
+        changeOrigin: true
+      }
+    }
+  }
 }
